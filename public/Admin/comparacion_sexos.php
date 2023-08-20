e75a6ad4afb45fb855afa99df386140135706cbc
ArthCA026 <r2chaves@gmail.com>
2023-08-19 19:23:08 -0600

    Mas codigo en comparacion

--- public/Admin/comparacion_sexos.php
+++ public/Admin/comparacion_sexos.php
@@ -11,3 +11,20 @@ if ($_SESSION['id_cargo'] != 1) {
 
     ocultarPagina();
 }
+
+include '../../db/conexion.php';
+
+// Query to get the count of appointments scheduled by males and females
+$sql = "SELECT Sexo, COUNT(*) AS Count FROM citas GROUP BY Sexo;";
+$result = mysqli_query($conexion, $sql);
+
+$sexoData = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $sexoData[$row['Sexo']] = $row['Count'];
+}
+
+// Close the database connection
+mysqli_close($conexion);
+
+$labels = array_keys($sexoData);
+$data = array_values($sexoData);
