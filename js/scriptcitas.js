2084aad4ebfa075b4443e8259c09e182147dc95b
jeff225588 <juegosbaratospirata194@gmail.com>
2023-08-20 11:32:43 -0600

    Arreglo de el boton de borrar datos en citas
    Cambios en el swich alert de agendar citas, se muestra primero antes que borrar datos

--- js/scriptcitas.js
+++ js/scriptcitas.js
@@ -1,38 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttonBorrarDatos = document.getElementById('borrar-datos');
-
-    buttonBorrarDatos.addEventListener('click', function() {
-        Swal.fire({
-            title: 'Confirmar',
-            text: '¿Estás seguro de que deseas borrar los datos ingresados?',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, borrar',
-            cancelButtonText: 'Cancelar'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                document.getElementById('booking-form').reset();
-                Swal.fire('Datos borrados', 'Los datos han sido borrados exitosamente', 'success');
-            }
-        });
-    });
-
     const buttonAgendarCita = document.getElementById('agendar-cita');
-
+    
     buttonAgendarCita.addEventListener('click', function(event) {
         event.preventDefault(); // Evitar que se envíe el formulario
-
+        
         const name = document.getElementById('name').value;
         const lastName = document.getElementById('last-name').value;
         const cedula = document.getElementById('cedula').value;
         const phone = document.getElementById('phone').value;
-        const datetime = document.getElementById('datetime').value; // Cambio aquí
+        const date = document.getElementById('datetime').value; // Corregido aquí
         const service = document.getElementById('service').value;
-
-        const messageText = `Nombres: ${name}\nApellidos: ${lastName}\nCédula: ${cedula}\nTeléfono: ${phone}\nFecha de cita: ${datetime}\nServicio: ${service}`;
-
+        
+        const messageText = `Nombres: ${name}\nApellidos: ${lastName}\nCédula: ${cedula}\nTeléfono: ${phone}\nFecha de cita: ${date}\nServicio: ${service}`; // Corregido aquí
+        
         Swal.fire({
             title: 'Confirmar',
             text: `¿Estás seguro de que deseas agendar la cita?\n\n${messageText}`,
@@ -50,7 +30,31 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-flatpickr("#datetime", {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttonBorrarDatos = document.getElementById('borrar-datos');
+
+    buttonBorrarDatos.addEventListener('click', function(event) {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del botón
+        
+        Swal.fire({
+            title: 'Confirmar',
+            text: '¿Estás seguro de que deseas borrar los datos ingresados?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, borrar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire('Datos borrados', 'Los datos han sido borrados exitosamente', 'success');
+                
+                // Retrasar el reseteo de los datos usando setTimeout
+                setTimeout(function() {
+                    document.getElementById('booking-form').reset();
+                }, 0);
+            }
+        });
+    });
 });
